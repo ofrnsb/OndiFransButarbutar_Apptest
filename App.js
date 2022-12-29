7949847f4f11411a29070173ff2460deaf061fcf
b <ondi.butarbutar@bni.co.id>
2022-12-29 16:03:53 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {Node} from 'react';
+import React from 'react';
+import {SafeAreaView, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import RootApp from './vendor/bundle/ruby/2.7.0/src/views/rootApp';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <RootApp />
+    </SafeAreaView>
+  );
+};
+
+export default App;
